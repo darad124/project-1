04dda69023e48e91fe2bf60d10097694ca95a1cc
darad124 <daryjoe765@gmail.com>
2023-09-10 19:34:51 +0100

    good

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  const slack_name = req.query.slack_name || "Gbolagade_Oluwadara";
-  const track = req.query.track || "backend";
+  const slack_name = "Gbolagade_Oluwadara";
+  const track = "backend";
   const current_day = new Date().toLocaleString('en-us', {  weekday: 'long' });
   const utc_time = new Date().toISOString();
   const github_file_url = 'https://github.com/darad124/project-1/blob/main/server.js';
