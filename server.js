97825a3ddcc0ec3f9959918a6dc472818dde3074
darad124 <daryjoe765@gmail.com>
2023-09-10 17:04:55 +0100

    last

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  const slack_name = req.query.slack_name;
-  const track = req.query.track;
+  const slack_name = "Gbolagade Oluwadara";
+  const track = "Backend";
   const current_day = new Date().toLocaleString('en-us', {  weekday: 'long' });
   const utc_time = new Date().toISOString();
   const github_file_url = 'https://github.com/darad124/project-1/blob/main/server.js';
